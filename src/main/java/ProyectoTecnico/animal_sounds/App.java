2e614da26df47a15cf9a0e31e2fa6f65cf25aacb
EJSOB <soberongarcia85@gmail.com>
2025-02-03 19:58:58 -0500

    Creacion del proyecto RetoTecnico

---
+++ src/main/java/ProyectoTecnico/animal_sounds/App.java
@@ -0,0 +1,13 @@
+package ProyectoTecnico.animal_sounds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
